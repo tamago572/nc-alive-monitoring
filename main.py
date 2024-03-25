b3c19b871844afa49fa1fb9074a12f018933c26c
potesala <110117095+tamago572@users.noreply.github.com>
2024-03-26 01:26:56 +0900

    -vzに変更

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def checkServerStatus(address:str, port):
     if address == None or port == None:
         return "Please enter the server adress and port"
     else:
-        response = subprocess.run(['nc', '-v', '-w', '1', str(address), str(port)], encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        response = subprocess.run(['nc', '-vz', str(address), str(port)], encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return "succeeded" in response.stderr # 出力結果はstderrに出力されるっぽい
 
-print(checkServerStatus("192.168.10.127", 25565))
+print(checkServerStatus("localhost", 3000))
