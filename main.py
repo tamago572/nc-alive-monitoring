9c8546b9515bbfcda64a6633d214a16ad0f8fad6
potesala <110117095+tamago572@users.noreply.github.com>
2024-03-25 16:57:02 +0900

    余計な処理をカット

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import subprocess
-import sys
 
 def checkServerStatus(address:str, port):
     if address == None or port == None:
         return "Please enter the server adress and port"
     else:
-        print("実行↓")
         response = subprocess.run(['nc', '-v', '-w', '1', str(address), str(port)], encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("stdout↓")
-        print(response.stdout)
-        print("stderr↓")
-        print(response.stderr)
-        return "succeeded" in response.stderr
+        return "succeeded" in response.stderr # 出力結果はstderrに出力されるっぽい
 
-print(checkServerStatus("192.168.10.127", 11451))
+print(checkServerStatus("192.168.10.127", 25565))
